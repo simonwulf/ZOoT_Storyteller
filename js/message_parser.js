a9974094202455b81a0169e039f59e1f1f2391e9
Simon Wulf <simonwulf.lund@bredband.net>
2016-07-27 22:59:40 +0200

    Break out message parsing and fix box break

---
+++ js/message_parser.js
@@ -0,0 +1,51 @@
+// message_parser.js
+
+'use strict';
+
+function MessageParser(plainCallback, commandCallback) {
+
+  const STATE_PLAIN = 0;
+  const STATE_SC_NAME = 1;
+  const STATE_SC_VALUE = 2;
+
+  var state = STATE_PLAIN;
+  var sc_name;
+  var sc_value;
+
+  this.parse = function (message) {
+    var caret = 0;
+    while (caret < message.length) {
+      var char = message[caret];
+      switch (state) {
+        case STATE_PLAIN:
+          if (char == '[') {
+            sc_name = '';
+            state = STATE_SC_NAME;
+          } else {
+            plainCallback(char);
+          }
+          break;
+        case STATE_SC_NAME:
+          if (char == ']') {
+            commandCallback(sc_name);
+            state = STATE_PLAIN;
+          } else if (char == ':') {
+            sc_value = '';
+            state = STATE_SC_VALUE;
+          } else {
+            sc_name += char;
+          }
+          break;
+        case STATE_SC_VALUE:
+          if (char == ']') {
+            commandCallback(sc_name, sc_value);
+            state = STATE_PLAIN;
+          } else {
+            sc_value += char;
+          }
+          break;
+      }
+      caret++;
+    }
+  }
+}
--- js/message_preview.js
+++ js/message_preview.js
@@ -1,60 +1,27 @@
 // message_preview.js
 
+'use strict';
+
 function MessagePreviewBuilder() {
 
   var currentColor = 'white';
   var preview;
   var currentBox;
   var boxes;
+  var parser = new MessageParser(
+    function (char) {
+      preview += char;
+    },
+    renderShortcode
+  );
 
   this.renderPreview = function (message) {
-
-    const STATE_PLAIN = 0;
-    const STATE_SC_NAME = 1;
-    const STATE_SC_VALUE = 2;
-
-    var index = 0;
-    var state = STATE_PLAIN;
-    var sc_name;
-    var sc_value;
 
     preview = '';
     currentBox = beginBox();
     boxes = [];
 
-    while (index < message.length) {
-      var char = message[index];
-      switch (state) {
-        case STATE_PLAIN:
-          if (char == '[') {
-            sc_name = '';
-            state = STATE_SC_NAME;
-          } else {
-            preview += char;
-          }
-          break;
-        case STATE_SC_NAME:
-          if (char == ']') {
-            preview += renderShortcode(sc_name);
-            state = STATE_PLAIN;
-          } else if (char == ':') {
-            sc_value = '';
-            state = STATE_SC_VALUE;
-          } else {
-            sc_name += char;
-          }
-          break;
-        case STATE_SC_VALUE:
-          if (char == ']') {
-            preview += renderShortcode(sc_name, sc_value);
-            state = STATE_PLAIN;
-          } else {
-            sc_value += char;
-          }
-          break;
-      }
-      index++;
-    }
+    parser.parse(message);
 
     endBox();
 
@@ -67,8 +34,6 @@ function MessagePreviewBuilder() {
     box.classList.add('message-preview-box');
     vfix.classList.add('message-valign-fix');
     box.appendChild(vfix);
-    console.log(currentColor);
-    console.log(preview);
     if (currentColor != 'white')
       preview += '<span class="color-' + currentColor + '">';
     return box;
@@ -99,27 +64,26 @@ function MessagePreviewBuilder() {
     return '<span class="space-strip" style="width:' + (count*2) + 'px;"></span>';
   }
 
-  function renderShortcode(name, value) {
+  function renderShortcode(command, value) {
 
-    switch (name) {
-      case 'break': endBox(); currentBox = beginBox(); break;
+    switch (command) {
+      case 'break':
       case 'delay': endBox(); currentBox = beginBox(); break;
-      case 'col': return renderColor(value);
-      case 'spaces': return renderSpaces(Number('0x'+value));
-      case 'name': return 'Link';
-      case 'A': return '<span class="icon-A"></span>';
-      case 'B': return '<span class="icon-B"></span>';
-      case 'C': return '<span class="icon-C"></span>';
-      case 'L': return '<span class="icon-L"></span>';
-      case 'R': return '<span class="icon-R"></span>';
-      case 'Z': return '<span class="icon-Z"></span>';
-      case 'Cu': return '<span class="icon-Up"></span>';
-      case 'Cd': return '<span class="icon-Down"></span>';
-      case 'Cl': return '<span class="icon-Left"></span>';
-      case 'Cr': return '<span class="icon-Right"></span>';
-      case 'Tri': return '<span class="icon-Triangle"></span>';
-      case 'Stick': return '<span class="icon-Stick"></span>';
-      default: return '';
+      case 'col': preview += renderColor(value); break;
+      case 'spaces': preview += renderSpaces(Number('0x'+value)); break;
+      case 'name': preview += 'Link'; break;
+      case 'A': preview += '<span class="icon-A"></span>'; break;
+      case 'B': preview += '<span class="icon-B"></span>'; break;
+      case 'C': preview += '<span class="icon-C"></span>'; break;
+      case 'L': preview += '<span class="icon-L"></span>'; break;
+      case 'R': preview += '<span class="icon-R"></span>'; break;
+      case 'Z': preview += '<span class="icon-Z"></span>'; break;
+      case 'Cu': preview += '<span class="icon-Up"></span>'; break;
+      case 'Cd': preview += '<span class="icon-Down"></span>'; break;
+      case 'Cl': preview += '<span class="icon-Left"></span>'; break;
+      case 'Cr': preview += '<span class="icon-Right"></span>'; break;
+      case 'Tri': preview += '<span class="icon-Triangle"></span>'; break;
+      case 'Stick': preview += '<span class="icon-Stick"></span>'; break;
     }
   }
 }
